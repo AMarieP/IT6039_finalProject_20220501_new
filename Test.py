427bacb57d20baf66570c6087efe0a38adfe8a3c
Alyssa <alyssa@kvasl.com>
2023-07-05 11:33:40 +1200

    Added Docstrings, renamed functon
    -Docstrings added for clarity on tests
    
    -The second testOneSpare was renamed to testAllSPare as the function actually tested for all spares thrown & was misnamed

--- Test.py
+++ Test.py
@@ -14,15 +14,40 @@ class TestBowlingGame(unittest.TestCase):
             self.game.rolls(pins)
 
     def testGutterGame(self):
+        '''
+        Tests a gutter game - a game where every ball gutters and no points are scored.
+
+        args: 
+        arg 1: Bowling Game  
+
+        Returns True if the score from self.game equals zero.
+        '''
         for i in range(0, 20):
             self.game.rolls(0)
         self.assertEqual(self.game.score(), 0, "Should be 0")
 
     def testAllOnes(self):
+        '''
+        Test score if every roll is a one
+
+        args: 
+        arg 1: self
+
+        Returns True if score from self.game equals 20.
+        '''
         self.rollMany(1, 20)
         self.assertEqual(self.game.score(), 20, "Should be 20")
 
     def testOneSpare(self):
+        '''
+        Test score if there is one spare rolled in the game, and
+        no other points are scored except for the spare and the doubled point roll. 
+
+        args:
+        arg 1: self
+
+        Returns True if score from self.game equals 16.
+        '''
         self.game.rolls(5)
         self.game.rolls(5)
         self.game.rolls(3)
@@ -30,6 +55,15 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(self.game.score(), 16, "Should be 16")
 
     def testOneStrike(self):
+        '''
+        Test score if there is one strike rolled in the game, and no other
+        points are scored except the strike and the two double point rolls. 
+
+        args: 
+        arg 1: self
+
+        Returns True if score from self.game equals 24.
+        '''
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
@@ -37,10 +71,26 @@ class TestBowlingGame(unittest.TestCase):
         self.assertEqual(self.game.score(), 24, "Should be 24")
 
     def testPerfectGame(self):
+        '''
+        Test result if player runs a perfect game of all strikes
+
+        args:
+        arg 1: self
+
+        Returns True if score from self.game equals 300
+        '''
         self.rollMany(10,12)
         self.assertEqual(self.game.score(), 300, "Should be 300")
 
-    def testOneSpare(self):
+    def testAllSpare(self):
+        '''
+        Test score if player rolls a game of all spares.
+
+        args:
+        arg 1: self
+
+        Returns True if self.game is equal to 150.
+        '''
         self.rollMany(5,21)
         self.assertEqual(self.game.score(), 150, "Should be 150")
 
