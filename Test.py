26be4a99bff8f581bd5fb46f7aaf7d6a7b7948af
Alyssa <alyssa@kvasl.com>
2023-07-05 11:24:09 +1200

    Restructured code layout and set up so I can run unit tests
    - Moved method rollMany to top of document under setUp as it is not a test and I wanted it seperate
    
    - Changed all asserts to assertEqual so I can have error message
    
    -Added line to initalize unittest

--- Test.py
+++ Test.py
@@ -9,37 +9,40 @@ class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
 
+    def rollMany(self, pins, rolls):
+        for i in range(rolls):
+            self.game.rolls(pins)
+
     def testGutterGame(self):
         for i in range(0, 20):
             self.game.rolls(0)
-        assert self.game.score()==0
+        self.assertEqual(self.game.score(), 0, "Should be 0")
 
     def testAllOnes(self):
         self.rollMany(1, 20)
-        assert self.game.score()==20
+        self.assertEqual(self.game.score(), 20, "Should be 20")
 
     def testOneSpare(self):
         self.game.rolls(5)
         self.game.rolls(5)
         self.game.rolls(3)
         self.rollMany(0,17)
-        assert self.game.score()==16
+        self.assertEqual(self.game.score(), 16, "Should be 16")
 
     def testOneStrike(self):
         self.game.rolls(10)
         self.game.rolls(4)
         self.game.rolls(3)
         self.rollMany(0,16)
-        assert  self.game.score()==24
+        self.assertEqual(self.game.score(), 24, "Should be 24")
 
     def testPerfectGame(self):
         self.rollMany(10,12)
-        assert self.game.score()==300
+        self.assertEqual(self.game.score(), 300, "Should be 300")
 
     def testOneSpare(self):
         self.rollMany(5,21)
-        assert self.game.score()==150
-        
-    def rollMany(self, pins,rolls):
-        for i in range(rolls):
-            self.game.rolls(pins)
+        self.assertEqual(self.game.score(), 150, "Should be 150")
+
+if __name__ == '__main__':
+    unittest.main()
